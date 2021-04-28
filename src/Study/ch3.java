014aff4281e682b02acb3578fa2df0f7dbc41082
chudeng <chudeng@hanmail.net>
2021-04-28 22:36:25 +0900

    집에서 업

--- src/Study/ch3.java
+++ src/Study/ch3.java
@@ -9,95 +9,182 @@ public class ch3 {
 		
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
-		//int num2 = sc.nextInt();
+		int num2 = sc.nextInt();
+		//int num3 = sc.nextInt();
+		//double dnum1 = sc.nextDouble();
+		//double dnum2 = sc.nextDouble();
 		//3-1
 		/*if (num < 0) {
-			System.out.println("ÀÌ °ªÀº À½ÀÇ °ªÀÔ´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ô´Ï´ï¿½.");
 		}
 		if (num > 0) {
-			System.out.println("ÀÌ °ªÀº ¾çÀÇ °ªÀÔ´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ô´Ï´ï¿½.");
 		}*/
 		
 		//3-2
 		/*if (num < 0) {
-			System.out.println(num + "ÀÇ Àý´ë°ª: " + (num * (-1)));
+			System.out.println(num + "ï¿½ï¿½ ï¿½ï¿½ï¿½ë°ª: " + (num * (-1)));
 		}
 		if (num > 0) {
-			System.out.println(num + "ÀÇ Àý´ë°ª: " + num);
+			System.out.println(num + "ï¿½ï¿½ ï¿½ï¿½ï¿½ë°ª: " + num);
 		}
-		System.out.println(num + "ÀÇ Àý´ë°ª: " + Math.abs(num));*/
+		System.out.println(num + "ï¿½ï¿½ ï¿½ï¿½ï¿½ë°ª: " + Math.abs(num));*/
 		
 		//3-3
 		/*if (num1%num2 == 0) {
-			System.out.println(num2 + "´Â " + num1 + "ÀÇ ¾à¼öÀÌ´Ù.");
+			System.out.println(num2 + "ï¿½ï¿½ " + num1 + "ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ì´ï¿½.");
 		} else {
-			System.out.println(num2 + "´Â " + num1 + "ÀÇ ¾à¼ö°¡ ¾Æ´Ï´Ù.");
+			System.out.println(num2 + "ï¿½ï¿½ " + num1 + "ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Æ´Ï´ï¿½.");
 		}*/
 		
 		//3-5
 		/*if (num1 < 0) {
-			System.out.println("ÀÌ °ªÀº À½ÀÇ °ªÀÔ´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ô´Ï´ï¿½.");
 		} else if (num1 == 0) {
-			System.out.println("ÀÌ °ªÀº 0 ÀÔ´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 0 ï¿½Ô´Ï´ï¿½.");
 		} else {
-			System.out.println("ÀÌ °ªÀº ¾çÀÇ °ªÀÔ´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ô´Ï´ï¿½.");
 		} */
 		
 		//3-7
 		/*if (num1 < num2) {
-			System.out.println(num1 + "ÀÌ Å©´Ù.");
+			System.out.println(num1 + "ï¿½ï¿½ Å©ï¿½ï¿½.");
 		} else if (num1 == num2) {
-			System.out.println(num1 + "°ú " + num2 + "°¡ °°´Ù.");
+			System.out.println(num1 + "ï¿½ï¿½ " + num2 + "ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½.");
 		} else {
-			System.out.println(num1 + "ÀÌ ÀÛ´Ù.");
+			System.out.println(num1 + "ï¿½ï¿½ ï¿½Û´ï¿½.");
 		} */
 		
 		//3-8
 		/*if (num1 < 0) {
-			System.out.println("À½ÀÇ Á¤¼ö°ªÀ» ÀÔ·Â Çß½À´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·ï¿½ ï¿½ß½ï¿½ï¿½Ï´ï¿½.");
 		} else if (num1%5 == 0) {
-			System.out.println("5·Î ³ª´©¾î ¶³¾îÁý´Ï´Ù.");
+			System.out.println("5ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï´ï¿½.");
 		} else {
-			System.out.println("5·Î ³ª´©¾î ¶³¾îÁöÁö ¾Ê½À´Ï´Ù.");
+			System.out.println("5ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ê½ï¿½ï¿½Ï´ï¿½.");
 		}*/
 		
 		//3-9
 		/*if (num1%10 == 0) {
-			System.out.println("10ÀÇ ¹è¼ö ÀÔ´Ï´Ù.");
+			System.out.println("10ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½Ô´Ï´ï¿½.");
 		} else {
-			System.out.println("10ÀÇ ¹è¼ö°¡ ¾Æ´Õ´Ï´Ù.");
+			System.out.println("10ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Æ´Õ´Ï´ï¿½.");
 		}*/
 		
 		//3-10
 		/*if (num1 < 0) {
-			System.out.println("¾ç¼ö°¡ ¾Æ´Ñ °ªÀ» ÀÔ·Â Çß½À´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Æ´ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·ï¿½ ï¿½ß½ï¿½ï¿½Ï´ï¿½.");
 		} else {
-			System.out.println("ÀÌ °ªÀº 3À¸·Î ³ª´©¾î Áý´Ï´Ù.");
+			System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 3ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ï´ï¿½.");
 			switch (num1%3) {
 			case 1:
-				System.out.println("ÀÌ °ªÀ» 3À¸·Î ³ª´« ³ª¸ÓÁö´Â 1ÀÔ´Ï´Ù.");
+				System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 3ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 1ï¿½Ô´Ï´ï¿½.");
 			case 2:
-				System.out.println("ÀÌ °ªÀ» 3À¸·Î ³ª´« ³ª¸ÓÁö´Â 2ÀÔ´Ï´Ù.");
+				System.out.println("ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 3ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 2ï¿½Ô´Ï´ï¿½.");
 			}
 		}*/
 		
 		//3-11
-		if (num1<0 || num1>100) {
-			System.out.println("Àß¸øµÈ Á¡¼ö ÀÔ´Ï´Ù.");
+		/*if (num1<0 || num1>100) {
+			System.out.println("ï¿½ß¸ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ô´Ï´ï¿½.");
 		}else {
 			if (num1 >= 80) {
-				System.out.println("¼ö");
+				System.out.println("ï¿½ï¿½");
 			}else if (num1 >= 70) {
-				System.out.println("¿ì");
+				System.out.println("ï¿½ï¿½");
 			}else if (num1 >= 60) {
-				System.out.println("¹Ì");
+				System.out.println("ï¿½ï¿½");
 			}else if (num1 >= 50) {
-				System.out.println("¾ç");
+				System.out.println("ï¿½ï¿½");
 			}else {
-				System.out.println("°¡");
+				System.out.println("ï¿½ï¿½");
 			}	
-		}
+		}*/
 		
+		//3-12
+		/*if (dnum1 > dnum2) {
+			System.out.println(dnum1);
+		} else {
+			System.out.println(dnum2);
+		}*/
+		
+		//3-13 ?? ë‚˜ì¤‘ì— ë‹¤ì‹œ í•´ë³´ì‚¼~
+		/*int diff = num1 - num2;
+		System.out.println(Math.abs(diff));*/
+		
+		//3-14
+		/*int diff = num1 - num2;
+		if (Math.abs(diff) > 10) {
+			System.out.println("ë‘ ê°’ì˜ ì°¨ëŠ” 11 ì´ìƒ");
+		} else {
+			System.out.println("ë‘ ê°’ì˜ ì°¨ëŠ” 10 ì´í•˜1");
+		}*/
+		
+		//3-15
+		/*if (num1 > num2) {
+			if (num2 > num3) {
+				System.out.println("ìµœì†Œê°’: " + num3);
+			} else {
+				System.out.println("ìµœì†Œê°’: " + num2);
+			}
+		}else {
+			if (num1 > num3) {
+				System.out.println("ìµœì†Œê°’: " + num3);
+			}else {
+				System.out.println("ìµœì†Œê°’: " + num1);
+			}
+		}*/
+		
+		//3-16
+		/*if (num1 > num2) {
+			if (num2 > num3) {
+				System.out.println("ì¤‘ì•™ê°’: " + num2);
+			}else if (num2 < num3) {
+				if(num3 > num1) {
+					System.out.println("ì¤‘ì•™ê°’: " + num1);
+				}else {
+					System.out.println("ì¤‘ì•™ê°’: " + num3);
+				}
+			}else {
+				System.out.println("ì¤‘ì•™ê°’: " + num2);
+			}
+		}else if (num1 < num2) {
+			if (num1 > num3) {
+				System.out.println("ì¤‘ì•™ê°’: " + num1);
+			}else if (num1 < num3) {
+				if (num2 > num3) {
+					System.out.println("ì¤‘ì•™ê°’: " + num3);
+				}else {
+					System.out.println("ì¤‘ì•™ê°’: " + num2);
+				}
+			}else {
+				System.out.println("ì¤‘ì•™ê°’: " + num1);
+			}
+		}else {
+			System.out.println("ì¤‘ì•™ê°’: " + num1);
+		}*/
+		
+		//3-17
+		/*if (num1 == num2) {
+			System.out.println("ë‘ ê°’ì€ ê°™ìŠµë‹ˆë‹¤.");
+		}else {
+			if (num1 > num2) {
+				System.out.println("í°ê°’ = " + num1 + "\nìž‘ì€ê°’ = " + num2);
+			}else {
+				System.out.println("í°ê°’ = " + num2 + "\nìž‘ì€ê°’ = " + num1);
+			}
+		}*/
+		
+		//3-18
+		int stk;
+		if (num1 >= num2) {
+			System.out.println("no chages");
+		}else {
+			stk = num1;
+			num1 = num2;
+			num2 = stk;
+			System.out.println("num1 and num2 are changed");
+		}
 		
 		sc.close();
 
---
+++ src/packages/example1/A.java
@@ -0,0 +1,11 @@
+package packages.example1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaaa");
+
+	}
+
+}
